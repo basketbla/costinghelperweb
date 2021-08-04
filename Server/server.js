2ccafd66f7666b5b89c07dbbdc1ed7dbef48701c
Rhett <rcowen@stanford.edu>
2021-08-03 23:01:16 -0500

    made everything work

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,126 @@
 const express = require('express');
-var cors = require('cors');
-const PORT =  process.env.PORT || 3000;
+const cors = require('cors');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const port =  process.env.PORT || 8080;
+var bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
+
+app.get('/', (req, res) => {
+	res.send('Server is running!');
+});
+
+app.post('/scrape', (req, res) => {
+	let items = req.body.items;
+	let numResults = req.body.numResults;
+	let results = [];
+
+	let promises = [];
+	for(let item of items){
+		promises.push(scrapeGoogle(item, numResults));
+	}
+
+	Promise.allSettled(promises)
+		.then((results) => {
+			console.log(results);
+			res.send(results);
+		})
+		.catch((e) => {
+			console.log(e)
+		})
+});
+
+//Function that actually does the meat of the scraping. Returns an object 
+//containing details for one item
+const scrapeGoogle = async (item, numResults) => {
+	try {
+		const { data } = await axios.get(
+			// 'https://www.google.com/search?psb=1&tbm=shop&q=' + item
+			'https://www.google.com/search?tbm=shop&q=' + item + '&hl=en&psb=1&ved=2ahUKEwiP1bX14pXyAhXjPgoDHQZTBcIQu-kFegQIABAT'
+		);
+		const $ = cheerio.load(data);
+
+		//Initialize lists
+		let items = [];
+		let prices = [];
+		let shipping = [];
+		let sellers = [];
+
+		//Scrape data
+		$('[class=rgHvZc]').each((index, value) => {
+			const itemName = $(value).text();
+			items.push(itemName);
+		});
+
+		$('[class=HRLxBb]').each((index, value) => {
+			const price = $(value).text();
+			prices.push(price);
+		});
+
+		$('span[class=dD8iuc]').each((index, value) => {
+			const shippingItem = $(value).text();
+			shipping.push(shippingItem);
+		});
+
+		$('div[class=dD8iuc]').each((index, value) => {
+			const seller = $(value).text();
+			sellers.push(seller);
+		});
+
+		//Adjust num results
+		let newNumResults = Math.min(numResults, items.length, prices.length, shipping.length, sellers.length);
+		let changed = false;
+		if (newNumResults < numResults) {
+			changed = true;
+		}
+
+		//Adjust the array lengths
+		items = items.slice(0, newNumResults);
+		prices = prices.slice(0, newNumResults);
+		shipping = shipping.slice(0, newNumResults);
+		sellers = sellers.slice(0, newNumResults);
+
+		//Do formatting
+		for(let i = 0; i < newNumResults; i++) {
+			prices[i] = Number(prices[i].substring(1).replace(',',''));
+			if (shipping[i] === 'Free shipping') {
+				shipping[i] = 0;
+			}
+			else {
+				shipping[i] = Number(shipping[i].substring(2, shipping[i].indexOf('s')-1).replace(','));
+			}
+			sellers[i] = sellers[i].substring(sellers[i].indexOf('from')+6)
+		}
+
+		let sumReducer = (a, b) => a + b;
+		let avgPrice = (prices.reduce(sumReducer, 0) + shipping.reduce(sumReducer, 0)) / newNumResults;
+		let returnItems = [];
+
+		//Generate return items
+		for(let i = 0; i < newNumResults; i++) {
+			returnItems.push({
+				'item' : items[i],
+				'price' : prices[i],
+				'shipping' : shipping[i],
+				'seller' : sellers[i]
+			});
+		}
+
+		return ({
+			'item' : item,
+			'numResults' : newNumResults,
+			'avgPrice' : avgPrice,
+			'itemList' : returnItems
+		})
+
+	} catch (error) {
+		throw error;
+	}
+};
---
+++ react-frontend/src/App.js
@@ -0,0 +1,205 @@
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import axios from 'axios';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Collapse from 'react-bootstrap/Collapse';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Spinner from 'react-bootstrap/Spinner';
+
+import './App.css';
+
+//Bootstrap styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { stringify } from 'qs';
+
+function App() {
+
+  const [fileText, setFileText] = useState('');
+  const [customOpen, setCustomOpen] = useState(false);
+  const [numResults, setNumResults] = useState(3);
+  const [returnData, setReturnData] = useState(null);
+  const [fileUploadLoading, setFileUploadLoading] = useState(false);
+  const [scraping, setScraping] = useState(false);
+
+  const nodeUrl = 'http://localhost:8080';
+
+  //Actually sends post request to node server
+  const doScraping = () => {
+    setScraping(true);
+    axios.post(nodeUrl + "/scrape", {
+      items: fileText.split("\n"),
+      numResults: numResults
+    }).then(
+      function(value) {
+        setReturnData(value.data);
+        setScraping(false);
+      },
+      function(error) {
+        setScraping(false);
+        alert(error + "\nPlease try again.");
+      }
+    );
+  }
+
+  //Formats object as a string and downloads as a txt file
+  const downloadResults = () => {
+    let returnString = "";
+    for (let chunk of returnData) {
+      console.log(chunk);
+      returnString += "Item: " + chunk.value.item + "\n";
+      if (chunk.value.numResults < numResults) {
+        returnString += "Was only able to find " + chunk.value.numResults + " instead of " + numResults + "\n";
+      }
+      returnString += "Average price: $" + Math.round(100*chunk.value.avgPrice)/100 + "\n\n";
+      for (let i = 0; i < chunk.value.numResults; i++) {
+        returnString += "Item " + parseInt(i+1) + ": " + chunk.value.itemList[i].item + "\n";
+        returnString += "Price: $" + chunk.value.itemList[i].price + "\n";
+        returnString += "Shipping: $" + chunk.value.itemList[i].shipping + "\n\n";
+      }
+      returnString += "\n";
+    }
+
+    const element = document.createElement("a");
+    const file = new Blob([returnString], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = "costedItems.txt";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  }
+
+  //Reads input file and sets file text based on it
+  const readFile = (e) => {
+    var reader = new FileReader();
+    setFileUploadLoading(true);
+    reader.onload = async (e) => {
+      setFileText(e.target.result);
+      setFileUploadLoading(false);
+    };
+  
+    if(e.target.files.length > 0) {
+      reader.readAsText(e.target.files[0]);
+    }
+    else {
+      setFileUploadLoading(false);
+      setFileText('');
+    }
+  }
+
+  let numberOptions = [...Array(20).keys()].map((number) => {
+    return (
+      <Dropdown.Item 
+        active={numResults === number + 1} 
+        onClick={() => setNumResults(number + 1)}
+        key={number + 1}
+      >
+        {number + 1}
+      </Dropdown.Item>
+    )
+  });
+
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Costing Helper</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#help">Help</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="section grey bottom-padding">
+        <div className="upload">
+          <Form.Group controlId="formFileLg" className="mb-3">
+            <Form.Label>Choose a .txt file</Form.Label>
+            <Form.Control 
+              type="file" 
+              size="lg" 
+              onChange={(e) => readFile(e)}
+              accept=".txt"
+              />
+          </Form.Group>
+          {
+            fileUploadLoading ? 
+              <Spinner animation="border" variant="danger" />
+            :
+              <div/>
+          }
+          <div id="or">Or</div>
+          <Button variant="danger" size="lg" onClick={() => setCustomOpen(!customOpen)}>Custom input</Button>
+          <Collapse in={customOpen}>
+            <Form.Group controlId="input-box">
+              <Form.Label>Enter items here (one per line)</Form.Label>
+              <Form.Control as="textarea" rows={3} value={fileText} onChange={(e) => setFileText(e.target.value)}/>
+            </Form.Group>
+          </Collapse>
+          {
+            !!fileText ?
+            <React.Fragment>
+              <DropdownButton size="lg" title={`Results: ${numResults}`} className='drop-button' variant='danger'>
+                {numberOptions}
+              </DropdownButton>
+              {
+                scraping ?
+                  <Spinner animation="border" variant="danger" style={{marginTop: '2rem'}}/>
+                :
+                  <React.Fragment>
+                    {
+                      !!returnData ?
+                        <Button variant="danger" size="lg" id="go-button" onClick={() => downloadResults()}>Download Results</Button>
+                      :
+                        <Button variant="danger" size="lg" id="go-button" onClick={() => doScraping()}>Go!</Button>
+                    }
+                  </React.Fragment>
+              }
+            </React.Fragment>
+            :
+            <div/>
+          }
+        </div>
+      </div>
+      <div className="section black" id="help">
+        <div className="text-container">
+          <div style={{fontSize: '3rem'}}>Welcome to Costing Helper!</div><br/>
+          Costing Helper is a tool that lets you cost items faster by automatically searching Google Shopping.
+          <br/>The steps are super simple:<br/><br/>
+          <ol>
+            <li>Upload a file with your items listed 1 per line (or type them in the custom input box)</li>
+            <li>Choose the number of each item you would like to include in the report</li>
+            <li>Hit 'Go!'</li>
+            <li>Download the text file with your results</li>
+          </ol>
+          <br/>
+          Since this site is making requests to Google Shopping, any variability in Google Shopping results will affect 
+          the results you receive through Costing Helper. For example, Google Shopping may return different items for the 
+          same search, or requests to Google Shopping may fail completely. Also, searches are made with the location of 
+          Fayetteville, AR (I think), so this should be the location that shipping prices are based on. 
+          <br/><br/>
+          Please reach out (<a className="link" href="mailto:pokerrangecalculator@gmail.com">pokerrangecalculator@gmail.com</a>) if you have any problems or ideas for improving the site! Ideas could be something like
+          <br/><ul>
+            <li>Support for CSV files for input/output</li>
+            <li>Support for making searches with a different location</li>
+            <li>Being able to search for more than 20 of each item</li>
+            <li>A menu that lets you select which results to keep and which to drop</li>
+            <li>Making this whole help section less ugly</li>
+          </ul>
+          Or anything else you can think of!
+        </div>
+        <div className="section grey" id="about">
+          <div className="text-container">
+            This website was built by <a className="link" href="https://www.rhett-owen.com/" target="_blank" rel="noreferrer">Rhett Owen</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
